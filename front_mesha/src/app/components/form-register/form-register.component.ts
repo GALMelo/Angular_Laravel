aad42032e7b52eb4a45700868a60f9cc2f47f679
GALMelo <gabriel.aflins@gmail.com>
2022-08-06 16:25:59 -0300

    sendo knowledges

--- front_mesha/src/app/components/form-register/form-register.component.ts
+++ front_mesha/src/app/components/form-register/form-register.component.ts
@@ -11,25 +11,46 @@ export class FormRegisterComponent implements OnInit {
   email = '';
   cpf = '';
   phone = '';
-  // knowledges: Array<any> = [
-  //   { name: 'Git', value: 'Git' },
-  //   { name: 'React', value: 'React' },
-  //   { name: 'PHP', value: 'PHP' },
-  //   { name: 'NodeJS', value: 'NodeJS' },
-  //   { name: 'DevOps', value: 'DevOps' },
-  //   { name: 'Banco de dados', value: 'Banco de dados' },
-  //   { name: 'TypeScript', value: 'Typescript' },
-  // ];
-  knowledges = 'PHP, TypeScript';
+  knowledges: Array<any> = [
+    { name: 'Git', value: 'Git', selected: false },
+    { name: 'React', value: 'React', selected: false },
+    { name: 'PHP', value: 'PHP', selected: false },
+    { name: 'NodeJS', value: 'NodeJS', selected: false },
+    { name: 'DevOps', value: 'DevOps', selected: false },
+    { name: 'Banco de dados', value: 'Banco de dados', selected: false },
+    { name: 'TypeScript', value: 'Typescript', selected: false },
+  ];
 
   constructor(private configService: ConfigService) {}
 
   ngOnInit(): void {}
+  onChangeKnowledge($event: any) {
+    const value = $event.target.value;
+    const isChecked = $event.target.checked;
+
+    this.knowledges = this.knowledges.map((know) => {
+      if (know.value === value) {
+        know.selected = isChecked;
+        return know;
+      }
+      return know;
+    });
+  }
 
   handleSubmit(event: Event): void {
+    let response = '';
+    this.knowledges.forEach((know) => {
+      if (know.selected) response += know.name + ', ';
+    });
     event.preventDefault();
     this.configService
-      .sendUser(this.name, this.email, this.cpf, this.phone, this.knowledges)
+      .sendUser(
+        this.name,
+        this.email,
+        this.cpf,
+        this.phone,
+        response.slice(0, response.length - 2)
+      )
       .subscribe((data) => {
         console.log(data);
       });
