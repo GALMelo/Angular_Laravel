dc99d1e92cb37b105a636d391d37cb181144d7ca
GALMelo <gabriel.aflins@gmail.com>
2022-08-06 15:02:09 -0300

    update form

--- front_mesha/src/app/config/config.service.ts
+++ front_mesha/src/app/config/config.service.ts
@@ -8,6 +8,21 @@ import { catchError, retry } from 'rxjs/operators';
 })
 export class ConfigService {
   constructor(private http: HttpClient) {}
+  sendUser(
+    name: string,
+    email: string,
+    cpf: string,
+    phone: string,
+    knowledge: string
+  ) {
+    return this.http.post('http://localhost:8000/api/users/register', {
+      name,
+      email,
+      cpf,
+      phone,
+      knowledge,
+    });
+  }
 
   getAllUsers() {
     return this.http.get('http://localhost:8000/api/users/all');
