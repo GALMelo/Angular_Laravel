3b23feae0e1028db3c2c14b552f0609d438d7757
GALMelo <gabriel.aflins@gmail.com>
2022-08-06 15:28:07 -0300

    create user

--- front_mesha/src/app/components/form-register/form-register.component.ts
+++ front_mesha/src/app/components/form-register/form-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from 'src/app/config/config.service';
 
 @Component({
   selector: 'form-register',
@@ -10,12 +11,27 @@ export class FormRegisterComponent implements OnInit {
   email = '';
   cpf = '';
   phone = '';
-  constructor() {}
+  // knowledges: Array<any> = [
+  //   { name: 'Git', value: 'Git' },
+  //   { name: 'React', value: 'React' },
+  //   { name: 'PHP', value: 'PHP' },
+  //   { name: 'NodeJS', value: 'NodeJS' },
+  //   { name: 'DevOps', value: 'DevOps' },
+  //   { name: 'Banco de dados', value: 'Banco de dados' },
+  //   { name: 'TypeScript', value: 'Typescript' },
+  // ];
+  knowledges = 'PHP, TypeScript';
+
+  constructor(private configService: ConfigService) {}
 
   ngOnInit(): void {}
 
   handleSubmit(event: Event): void {
     event.preventDefault();
-    console.log(this.name, this.email, this.cpf, this.phone);
+    this.configService
+      .sendUser(this.name, this.email, this.cpf, this.phone, this.knowledges)
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
 }
