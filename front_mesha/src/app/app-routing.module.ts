7fd0af49386b8b04014c0adb5c073ea2b5e3b46a
GALMelo <gabriel.aflins@gmail.com>
2022-08-06 13:38:20 -0300

    add route register

--- front_mesha/src/app/app-routing.module.ts
+++ front_mesha/src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewAllComponent } from './users/view-all/view-all.component';
 import { ViewUserComponent } from './users/view-user/view-user.component';
+import { RegisterUserComponent } from './views/register-user/register-user.component';
 
 const routes: Routes = [
+  {
+    path: ':nomedocara/registrar',
+    component: RegisterUserComponent,
+  },
   { path: 'registros', component: ViewAllComponent },
   { path: ':NOMECOLABORADOR/validar', component: ViewUserComponent },
 ];
