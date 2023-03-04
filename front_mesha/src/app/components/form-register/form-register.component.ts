611bed5acaea46d675a67140fd76d9f6bed96596
GALMelo <gabriel.aflins@gmail.com>
2023-03-04 17:05:52 -0300

    new features front

--- front_mesha/src/app/components/form-register/form-register.component.ts
+++ front_mesha/src/app/components/form-register/form-register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigService } from 'src/app/config/config.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'form-register',
@@ -31,7 +32,7 @@ export class FormRegisterComponent implements OnInit {
     },
   ];
 
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService, private router: Router) {}
 
   ngOnInit(): void {}
   onChangeKnowledge($event: any) {
@@ -68,22 +69,89 @@ export class FormRegisterComponent implements OnInit {
     });
   }
 
+  cpfValidator(cpf: string) {
+    // Remove qualquer caracter que não seja número do CPF
+    const cpfLimpo = this.cpf.replace(/[^\d]+/g, '');
+
+    // Verifica se o CPF possui 11 dígitos
+    if (cpfLimpo.length !== 11) {
+      return false;
+    }
+
+    // Calcula o primeiro dígito verificador
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+      soma += parseInt(cpfLimpo.charAt(i)) * (10 - i);
+    }
+    let resto = 11 - (soma % 11);
+    const primeiroDV = resto === 10 || resto === 11 ? 0 : resto;
+
+    // Calcula o segundo dígito verificador
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+      soma += parseInt(cpfLimpo.charAt(i)) * (11 - i);
+    }
+    resto = 11 - (soma % 11);
+    const segundoDV = resto === 10 || resto === 11 ? 0 : resto;
+
+    // Verifica se os dígitos verificadores calculados são iguais aos dígitos verificadores do CPF
+    if (
+      parseInt(cpfLimpo.charAt(9)) === primeiroDV &&
+      parseInt(cpfLimpo.charAt(10)) === segundoDV
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  emailValidator(email: string) {
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    return emailRegex.test(email);
+  }
+
   handleSubmit(event: Event): void {
     let response = '';
     this.knowledges.forEach((know) => {
       if (know.selected) response += know.name + ', ';
     });
     event.preventDefault();
-    this.configService
-      .sendUser(
-        this.name,
-        this.email,
-        this.cpf,
-        this.phone,
-        response.slice(0, response.length - 2)
-      )
-      .subscribe((data) => {
-        console.log(data);
-      });
+    if (
+      this.cpfValidator(this.cpf) == true &&
+      this.emailValidator(this.email) == true
+    ) {
+      this.configService
+        .sendUser(
+          this.name,
+          this.email,
+          this.cpf,
+          this.phone,
+          response.slice(0, response.length - 2)
+        )
+        .subscribe(
+          () => {
+            this.router.navigate(['/registros']);
+          },
+          (error) => {
+            console.log(error.error.message);
+            if (error.error.message === 'CPF inválido') {
+              alert('CPF inválido');
+            }
+            if (error.error.message === 'Email inválido') {
+              alert('Email inválido');
+            }
+            if (error.error.message === 'Usuário já existe') {
+              alert('Usuário já existe');
+            }
+          }
+        );
+    } else {
+      if (this.cpfValidator(this.cpf) == false) {
+        alert('CPF inválido');
+      }
+      if (this.emailValidator(this.email) == false) {
+        alert('Email inválido');
+      }
+    }
   }
 }
